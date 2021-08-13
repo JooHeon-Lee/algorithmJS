415dd39826038a7b94de4584a32b70b0fca3d564
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2021-08-13 23:20:01 +0900

    0813

---
+++ 프로그래머스/정렬_가장큰수.js
@@ -0,0 +1,6 @@
+const solution = (numbers) => {
+    let list = [];
+    let answer = numbers.map(c => c + '')
+    .sort((a,b) => (b+a) - (a+b)).join('');
+    return answer[0]==='0'? '0' : answer;
+}
