bb4627d733b107456d2416d0cba253d499fbc2a5
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-08-28 18:29:30 +0900

    0828

--- 그래프/트리/BFS2.js
+++ 그래프/트리/BFS2.js
@@ -2,14 +2,9 @@
 const graph = {
     A: ["B", "C"],
     B: ["A", "D"],
-    C: ["A", "G", "H", "I"],
-    D: ["B", "E", "F"],
-    E: ["D"],
-    F: ["D"],
-    G: ["C"],
-    H: ["C"],
-    I: ["C", "J"],
-    J: ["I"]
+    C: ["A", "D", "F"],
+    D: ["B", "C", "F"],
+    F: ["C","D"]
   };
   
   const BFS = (graph, startNode) => {
