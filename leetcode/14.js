ca2896deb27fe8a9847b5109beabfc145692d628
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-09-06 21:30:27 +0900

    0906

--- leetcode/14.js
+++ leetcode/14.js
@@ -4,21 +4,20 @@ Output: "fl"
  * @param {array} strs 
  */
 
+//str 하나를 기준으로 str[][] 요소 다 비교. false 되면 slice
 const solution = (strs) => {
 
     if(!strs.length) return '';
     
     for(let i = 0; i < strs[0].length; i++) { // i = f, l, o, w, e, r
         for(let s of strs) { // s = flower, flow, flight
+
             if(s[i] !== strs[0][i]) {
                 return s.slice(0, i);
             }
         }
-    }   
+    }
     return strs[0];
-
-
 }
 
-console.log(["flower","flow","flight"])
-console.log(["flow"] in ["flower","flow"])
+solution(["flower","flow","flight"])
