b4244150620ab0841f7eb5c04064d194f539733d
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-09-17 22:00:29 +0900

    0917

--- leetcode/15.js
+++ leetcode/15.js
@@ -53,3 +53,35 @@ const solution = (nums) => {
 
 
 solution([-1,0,1,2,-1,-4])
+
+
+
+// 속도 개선
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let ret = [];
+  
+    for(let i = 0; i < nums.length - 2; ++i) {
+      if(i > 0 && nums[i] === nums[i - 1]) continue;
+  
+      let j = i + 1;
+      let k = nums.length - 1;
+      while(j < k) {
+        let sum = nums[i] + nums[j] + nums[k];
+        if(sum === 0) {
+          ret.push([nums[i], nums[j], nums[k]]);
+          do {j++} while(j < k && nums[j] === nums[j - 1]);
+          do {k--} while(j < k && nums[k] === nums[k + 1]);
+        } 
+        else if(sum < 0) j++;
+        else if(sum > 0) k--; 
+      }
+    }
+  
+    return ret;
+  
+  };
