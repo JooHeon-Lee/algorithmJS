c8aa2edac4c7a6cdd37bfacbb8691e71ef7f7ce9
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-10-15 16:03:04 +0900

    1015

---
+++ leetcode/118-re.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+ var generate = function(numRows) {
+    var i = 0;
+  var j = 0;
+  var res = [];
+  for (i = 0; i < numRows; i++) {
+    res.push(Array(i + 1));
+    for (j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        res[i][j] = 1;
+      } else {
+        res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+      }
+    }
+  }
+  return res;
+};
